ae35fc0f71419c07060e4f507d5e02fbfd495154
Ali-Azimi00 <alidotazimi@gmail.com>
2023-04-15 12:15:37 -0700

    uncommented react router dom, added redirect file to public folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { React } from 'react';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 
 import { Welcome } from './pages';
@@ -9,15 +9,13 @@ import { Welcome } from './pages';
 const App = () => {
 
   return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />}> </Route>
+        {/* <Route path="/GA" element={<Analytics />} ></Route> */}
+      </Routes>
 
-    <Welcome />
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<Welcome />}> </Route>
-    //     {/* <Route path="/GA" element={<Analytics />} ></Route> */}
-    //   </Routes>
-
-    // </BrowserRouter>
+    </BrowserRouter>
   )
 }
 
