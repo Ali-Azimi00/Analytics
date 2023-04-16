3f8e4ecff10093327a904d591babf42caf3277ec
Ali-Azimi00 <alidotazimi@gmail.com>
2023-04-15 19:47:41 -0700

    uncommented state options

--- src/components/drawerComp.jsx
+++ src/components/drawerComp.jsx
@@ -3,8 +3,8 @@ import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 // import Button from '@mui/material/Button';
 
-// import Icon from '@mdi/react';
-// import { mdiText } from '@mdi/js';
+import Icon from '@mdi/react';
+import { mdiText } from '@mdi/js';
 
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
@@ -21,9 +21,9 @@ import '../pages/analytics.css'
 const TemporaryDrawer = () => {
     const [state, setState] = useState({
         top: false,
-        // left: false,
-        // bottom: false,
-        // right: false,
+        left: false,
+        bottom: false,
+        right: false,
     });
 
     const toggleDrawer = (anchor, open) => (event) => {
@@ -39,7 +39,7 @@ const TemporaryDrawer = () => {
             sx={{
                 display: 'inline-flex',
                 flexWrap: 'wrap',
-                // width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250,
+                width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250,
                 color: 'gray',
                 backgroundColor: '#1a1a1a',
                 // width: 250,
@@ -112,81 +112,7 @@ const TemporaryDrawer = () => {
 
     return (
         <div>
-
-            <Box
-                sx={{
-                    display: 'inline-flex',
-                    flexWrap: 'wrap',
-                    // width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250,
-                    color: 'gray',
-                    backgroundColor: '#1a1a1a',
-                    // width: 250,
-                    height: '100%',
-                    // maxHeight: "100%",
-                    backgroundImage: "../assets/polygonPattern.png",
-                    overflow: 'hidden',
-
-                    justifyContent: 'space-evenly'
-
-                }}
-            // role="presentation"
-            // onClick={toggleDrawer(anchor, false)}
-            // onKeyDown={toggleDrawer(anchor, false)}
-            >
-                <div style={{}}>
-                    <div>
-                        <div className="drawerItem" >
-                            <img src={logo} alt="logo" style={{ width: '150px', height: '150px' }} className='logo' />
-                            <div style={{
-                                marginTop: '5%',
-                                marginBottom: '5%'
-                            }}>
-                                Ali Azimi
-                            </div>
-                            <div style={{
-                                marginTop: '5%',
-                                marginBottom: '5%'
-                            }}>
-                                Full-Stack Developer
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <Divider color='#00E396' inset='3em' />
-
-                    <List style={{
-                        marginTop: ''
-                    }}>
-                        {['Google Analytics', 'Stocks', 'Cyrpto'].map((text, index) => (
-                            <ListItem key={text} disablePadding className='aButton'>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                    </ListItemIcon>
-                                    <ListItemText primary={text} />
-                                </ListItemButton>
-                            </ListItem>
-                        ))}
-                    </List>
-
-                    <Divider color='#00E396' />
-
-
-                    <div className='drawerItem' >
-                        <div style={{
-                            marginTop: '5%'
-                        }}>
-                            Contact
-                        </div>
-                        <div style={{ fontSize: '14px', marginTop: '10px' }}>
-                            azimi-swe@gmail.com
-                        </div>
-                    </div>
-
-                </div>
-            </Box>
-            {/* {['left'].map((anchor) => (
+            {['left'].map((anchor) => (
                 <div key={anchor}>
 
                     <div onClick={toggleDrawer(anchor, true)}
@@ -204,7 +130,7 @@ const TemporaryDrawer = () => {
                     </Drawer>
                 </div>
             ))
-            } */}
+            }
         </div >
     );
 }
