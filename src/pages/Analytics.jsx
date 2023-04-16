fda32287f665d0dcbd1ae0de510c4baa58388151
Ali-Azimi00 <alidotazimi@gmail.com>
2023-04-15 19:08:05 -0700

    commented out GA

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import GA from "./GoogleAnalytics"
+// import GA from "./GoogleAnalytics"
 import NavBar from "../components/navBar"
 
 
@@ -8,7 +8,7 @@ const Analytics = () => {
         <div>
             <NavBar></NavBar>
             <div style={{ marginTop: '5em' }} >
-                <GA></GA>
+                {/* <GA></GA> */}
             </div>
         </div>
     )
